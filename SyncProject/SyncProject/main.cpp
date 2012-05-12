d75ccad0c1df0c1ad9570bf6fbd6e88818a90d93
AJ <theprofessionalhub@gmail.com>
2012-05-11 20:53:36 -0500

    Initial code xcode checkin

---
+++ SyncProject/SyncProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SyncProject
+//
+//  Created by Arjun Karthik Venugopal on 5/11/12.
+//  Copyright (c) 2012 AMD. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
